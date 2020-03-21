92c7e8a34f38a5716fa7dd9036e7a601cf49b042
Tim Guenther <4759569+TimGuenther@users.noreply.github.com>
2020-03-21 16:24:14 -0400

    First idea of website

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function search(text){
+  //Start search request *Wait for Backend*
+  json = "";
+  return json;
+}
+
+function sendToMainSearch(input){
+  window.location.assign("/search/#input="+input);
+}
+
+window.onload = function(){
+  //register click handler
+  $("#button-addon-search").click(function(obj){
+    value = $("#button-addon-search-input").val()
+    if(value !== undefined || value !== ""){
+      sendToMainSearch($("#button-addon-search-input").val());
+    }
+  });
+  $("#button-navbar-search").click(function(obj){
+    value = $("#button-navbar-search-input").val()
+    if(value !== undefined || value !== ""){
+      sendToMainSearch($("#button-navbar-search-input").val());
+    }
+  });
+};
